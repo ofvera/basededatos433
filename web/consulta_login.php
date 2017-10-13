b8c38a4f9b5c3eaa17a704c376b9536ba8595662
Paula Salvo <pcsalvo@uc.cl>
2017-10-13 17:32:30 -0300

    consulta

---
+++ web/consulta_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<?php
+include('head.php');
+ ?>
+
+<!-- aca va la consulta -->
+
+
+<form action="index.php" method="post">
+  <input type="submit" value="Volver">
+</form>
+
+<?php
+include('final.php');
+ ?>
