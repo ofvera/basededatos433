db178dc2523e92b8fe9ba5fb67931acca322d163
Paula Salvo <pcsalvo@uc.cl>
2017-10-16 01:57:18 -0300

    login

---
+++ web/consulta_login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<?php
+include('head.php');
+ ?>
+
+<!-- aca va la consulta -->
+<?php 
+	require('conexion.php');
+
+	$usuario = $_POST['usuario']
+	$clave = $_POST['clave']
+
+	$query = 'SELECT user, clave FROM Usuario WHERE user = $usuario AND clave = $clave;';
+
+	$result = $db33 -> prepare($query);
+	$result -> execute();
+	$dataCollected = $result -> fetch();
+
+	if (is_null($dataCollected)){
+		echo "El usuario o clave incorrecta";
+		echo "<form action="index.php" method="post">
+			<input type="submit" value="Volver">
+			</form>";
+	}
+
+	else {
+		echo "<form action="usuario.php" method="post">
+			<input type="submit" value="Volver">
+			</form>";
+	}
+	
+ ?>
