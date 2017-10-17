563fdd5cf9a025419358ddf61153be6f37419a92
Octavio Vera <ofvera@uc.cl>
2017-10-16 23:14:09 -0300

    concierto_banda

--- web/consulta_concierto_banda.php
+++ web/consulta_concierto_banda.php
@@ -14,37 +14,52 @@ include('head.php');
 
 	if is_null($fecha_inicio) or is_null($fecha_termino){
 
-	echo "Debes ingresar ambas fechas para realizar la consulta"
+		echo "Debes ingresar ambas fechas para realizar la consulta"
 
 	}
 
 	else {
 		if is_null($banda){
-			$querry = "SELECT C.nombre FROM concierto C WHERE C.fecha > '$fecha_inicio' AND C.fecha < '$fecha_termino'";
+			$querry = "SELECT C.nombre, C.fecha FROM concierto C WHERE C.fecha > '$fecha_inicio' AND C.fecha < '$fecha_termino';";
 		}
 		
 		else {
-			$querry = "SELECT C.nombre FROM banda B, banda_invitada BI, concierto_banda CB, concierto C WHERE B.nombre = '$banda' AND ((B.id_b = BI.id_b AND BI.id_c = C.id_c) OR (B.id_b = CB.id_b AND CB.id_c = C.id_c)) AND C.fecha > '$fecha_inicio' AND C.fecha < '$fecha_termino'";
-		}
-	
-	$result = $db4 -> prepare($querry);
-	$result -> execute();
-	$dataCollected = $result -> fetch();
+			$querry = "SELECT C.nombre C.fecha FROM banda B, banda_invitada BI, concierto C WHERE B.nombre = '$banda' AND B.id_b = BI.id_b AND BI.id_c = C.id_c AND C.fecha > '$fecha_inicio' AND C.fecha < '$fecha_termino'
+				UNION 
+				SELECT C.nombre, C.fecha FROM banda B, concierto_banda CB, concierto C WHERE B.nombre = '$banda' AND B.id_b = CB.id_b AND CB.id_c = C.id_c AND C.fecha > '$fecha_inicio' AND C.fecha < '$fecha_termino';"
 
-	<?php
-    echo "<h3>Integrantes actuales</h3>
-    <table>
-    <tr> <td>Nombre</td> <td>Correo</td> </tr>";
-    foreach ($data as $p) {
-        if ($p[integrante_actual] <> '') {
-            echo "<tr>
-            <td>$p[integrante_actual]</td>
-            <td>$p[correo]</td>
-            </tr>";
-        }
-    }
+			## no estoy seguro de esto ##
 
-    ?>
+			$result = $db4 -> prepare($querry);
+			$result -> execute();
+			$dataCollected = $result -> fetch();
 
-	}
+			if (empty($querry)){
+				echo "No hay conciertos en esas fechas"
+			}
+			else {
+				echo "<h3>Conciertos</h3>
+   				<table>
+    			<tr> <td>Nombre</td> <td>Fecha</td> </tr>";
+    			foreach ($querry as $p) {
+        	   		echo "<tr>
+            		<td>$p[0]</td>
+            		<td>$p[1]</td>
+            		</tr>";
+        
+    			}	
+			}
+			
+			## hasta aca ##
+		}	
+?>
+
+
+<form action="usuario.php" method="post">
+    <input type="submit" value="Volver">
+</form>
+
+<?php
+include('footer.php');
+?>
 	
